337916dd7c883def12274a5d739e7ff9ebd14e21
Ivan <ivan.teaca@gmail.com>
2023-01-27 15:34:47 +0200

    Add avatar for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?  
   
   protected
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:fullname, :phone, :email, :password, :avatar)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:fullname, :phone, :avatar, :email, :password, :current_password)}
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,25 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :apartments
-  
+
+  has_one_attached :avatar
+
+  after_commit :add_deafult_avatar, on: %i[create update]
+
+  private
+
+  def add_deafult_avatar 
+    unless avatar.attached?
+      avatar.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'deafult.jpg'
+          )
+        ), 
+        filename: 'deafult.jpg',
+        content_type: 'image/jpg'
+      ) 
+    end
+  end
 end
+
